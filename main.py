2752efe8edcc92c5862594e76e2f17d61c9d1187
Scorpiana88 <84060167+Scorpiana88@users.noreply.github.com>
2022-03-24 13:16:56 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+ 
+##  CNE 350 Final Project
+##  Instructor: Justin Eliis,
+##  Christiana Freeman, csfreeman@student.rtc.edu
+##  with help from TJ Dewey, tjdewey@student.rtc.edu
+
+
+
+from scapy.all import *
+import json
+import threading
+import http.client
+
